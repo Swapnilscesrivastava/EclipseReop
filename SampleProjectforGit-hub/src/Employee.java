4b0dd211ae22ac476dc2b9e466e56fbe5e897bd4
Swapnil <swapnilscesrivastava@gmail.com>
2020-10-28 20:16:25 +0530

    cor

---
+++ SampleProjectforGit-hub/src/Employee.java
@@ -0,0 +1,10 @@
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		System.out.println("Employee");
+		System.out.println("Employee Data <>/,?");
+	}
+
+}
