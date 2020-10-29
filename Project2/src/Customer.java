ca61cd2322d29b1e3b331f84154cf37d47133d19
Swapnil <swapnilscesrivastava@gmail.com>
2020-10-29 11:01:20 +0530

    commufyu

---
+++ Project2/src/Customer.java
@@ -0,0 +1,10 @@
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		System.out.println("Customer");
+		System.out.println("Customer Data <>/,?");
+	}
+
+}
